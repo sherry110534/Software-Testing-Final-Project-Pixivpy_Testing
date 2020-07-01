cb4683d770237e5dc48518c2532c09f809ebf7b9
Du Jin <sherry110534@gmail.com>
2020-07-01 13:52:58 +0800

    add task testing

---
+++ src/test/java/coflowsim/datastructures/TaskTest.java
@@ -0,0 +1,42 @@
+package coflowsim.datastructures;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void startTask() {
+    }
+
+    @Test
+    void cleanupTask() {
+    }
+
+    @Test
+    void getPlacement() {
+    }
+
+    @Test
+    void hasStarted() {
+    }
+
+    @Test
+    void isCompleted() {
+    }
+
+    @Test
+    void resetTaskStates() {
+    }
+}
